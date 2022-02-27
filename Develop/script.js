dc215311bbbf6b40c70bdace4bd31348f7171aa7
soulslurpee <charles.d.thompson87@gmail.com>
2022-02-27 09:44:50 -0500

    initial commit

--- Develop/script.js
+++ Develop/script.js
@@ -14,4 +14,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
