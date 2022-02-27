040dc1a7394af48f8dc6a7e4af8a4d936995fa77
soulslurpee <charles.d.thompson87@gmail.com>
2022-02-27 10:25:31 -0500

    added intial user prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,11 @@
 // Assignment code here
+const passwordLength = Number(prompt("What is the length of your password? (8 - 128 Characters)"));
+const hasLowercase = confirm("Does the password require lowercase characters?");
+const hasUppercase = confirm("Does the password require uppercase characters?");
+const hasNumeric = confirm("Does the password require numeric characters?");
+const hasSpecial = confirm("Does the password require special characters?");
 
+console.log(passwordLength, hasLowercase, hasUppercase, hasNumeric, hasSpecial);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -8,6 +14,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
 
   passwordText.value = password;
 
